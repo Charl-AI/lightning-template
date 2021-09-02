5e82bbfd6d4c8ce1a0bd80f56e43eafb86a7fd39
charlie <charlesjones9991@gmail.com>
2021-09-03 00:19:00 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='project',
-    version='0.0.0',
-    description='Describe Your Cool Project',
-    author='',
-    author_email='',
+    name="project",
+    version="0.0.0",
+    description="Describe Your Cool Project",
+    author="Charles Jones",
+    author_email="",
     # REPLACE WITH YOUR OWN GITHUB PROJECT LINK
-    url='https://github.com/PyTorchLightning/pytorch-lightning-conference-seed',
-    install_requires=['pytorch-lightning'],
-    packages=find_packages(),
+    url="https://github.com/Charl-AI/PROJECT-NAME",
+    install_requires=["pytorch-lightning"],
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
 )
-
