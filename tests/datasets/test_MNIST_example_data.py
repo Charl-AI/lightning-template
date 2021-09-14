b431cada62181eb3253e376f4d136a8cf4829c68
charlie <charlesjones9991@gmail.com>
2021-09-14 14:55:03 +0100

    rename dataset test file and create skeleton dataset tests

--- tests/datasets/test_MNIST_example_data.py
+++ tests/datasets/test_MNIST_example_data.py
@@ -1,8 +1,5 @@
 from tests.test_suite_utils.local_test import local_test
 
-# this file contains an example test for the
-# rename this file to match its custom dataset equivalent... e.g. test_tomography.py
-
 
 def test_example():
     # this test isn't very useful...
@@ -12,4 +9,4 @@ def test_example():
 # This test wont run on Actions CI
 @local_test
 def test_example_local():
-    assert False
+    assert True
