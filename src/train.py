184a9394c0a9691db0616396cfb1c584f143279a
Charlie <charlesjones9991@gmail.com>
2021-09-20 20:12:24 +0100

    add alert and title args to train

--- src/train.py
+++ src/train.py
@@ -9,6 +9,8 @@ from visualisation.wandb_callbacks.image_classification_callback import (
 )
 from models.MNIST_example_model import MNISTResNetModule
 from datasets.MNIST_example_data import MNISTDataModule
+from utils import get_project_root_name
+import wandb
 
 seed_everything(1)
 
@@ -20,7 +22,14 @@ def main(args):
     model = MNISTResNetModule(lr=dict_args["learning_rate"])
     data = MNISTDataModule(batch_size=dict_args["batch_size"])
 
-    wandb_logger = WandbLogger(name=f"{input('Title of run:' )}", log_model="all")
+    if args.title:
+        title = f"{input('Title of run:' )}"
+    else:
+        title = ""
+
+    wandb_logger = WandbLogger(
+        name=f"{title}", log_model="all", project=f"{get_project_root_name()}-logs"
+    )
     wandb_logger.watch(model)
 
     trainer = pl.Trainer.from_argparse_args(
@@ -30,10 +39,31 @@ def main(args):
     )
     trainer.fit(model, data)
 
+    if args.alert:
+        wandb.alert(title="Run Finished", text="Login to W&B to see results :)")
+
+
+def add_program_specific_args(parent_parser):
+
+    parser = parent_parser.add_argument_group("Program Arguments")
+    parser.add_argument(
+        "--alert",
+        action="store_true",
+        help="Send E-mail alert when finished. Relys on Weights and Biases Alerts.",
+    )
+
+    parser.add_argument(
+        "--title",
+        action="store_true",
+        help="Enable title prompt",
+    )
+    return parent_parser
+
 
 if __name__ == "__main__":
 
     parser = ArgumentParser()
+    parser = add_program_specific_args(parser)
     parser = MNISTResNetModule.add_model_specific_args(parser)
     parser = MNISTDataModule.add_dataset_specific_args(parser)
     parser = pl.Trainer.add_argparse_args(parser)
